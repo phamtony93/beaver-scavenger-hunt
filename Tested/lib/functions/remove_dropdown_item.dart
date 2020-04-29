58767503fab436f1da85795c56a88e441f308b07
Jake Seawell <44551335+JSeawell@users.noreply.github.com>
2020-04-29 16:21:18 -0700

    Delete remove_dropdown_item.dart

--- Tested/lib/functions/remove_dropdown_item.dart
+++
@@ -1,7 +0,0 @@
-removeDropdownItem(String item, dropdownDataList){
-    for (int i = 0; i < dropdownDataList.length; i++){
-      if (dropdownDataList[i]["display"] == item){
-        dropdownDataList.removeAt(i);
-      }
-    }
-  }
