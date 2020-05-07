3851cef78bb33263b304c94d9ecc40611583d8b6
Jake Seawell <44551335+JSeawell@users.noreply.github.com>
2020-05-07 13:14:51 -0700

    Update join_game_screen.dart

--- beaver_scavenger_hunt/lib/screens/join_game_screen.dart
+++ beaver_scavenger_hunt/lib/screens/join_game_screen.dart
@@ -22,10 +22,6 @@ class _JoinGameScreen extends State<JoinGameScreen> {
   String gameCode;
 
   void addPlayerToGame(String playerID, String gameID) async {
-<<<<<<< HEAD
-    // var ref = Firestore.instance.collection('games').document(gameID).get();
-    Firestore.instance.collection('games').document(gameID).updateData({'playerID': FieldValue.arrayUnion([playerID])});
-=======
     Firestore.instance.collection("users").document("$playerID").updateData({'gameID': '$gameCode'});
     Firestore.instance.collection('games').document("$gameID").updateData({'playerIDs': FieldValue.arrayUnion([playerID])});
   }
@@ -39,7 +35,6 @@ class _JoinGameScreen extends State<JoinGameScreen> {
       return 'Please enter your 4 digit code';
     }
     return null;
->>>>>>> seawellj_branch
   }
 
   void submitForm(formKey) {
@@ -80,19 +75,7 @@ class _JoinGameScreen extends State<JoinGameScreen> {
                     ),
                   ),
                   // maxLength: 6,
-<<<<<<< HEAD
-                  validator: (value) {
-                    if (value.isEmpty) {
-                      return 'Please enter your 4 digit code';
-                    }
-                    if (value.length != 4) {
-                      return 'Please enter your 4 digit code';
-                    }
-                    return null;
-                  },
-=======
                   validator: (value) => _validateGameCode(value),
->>>>>>> seawellj_branch
                   onSaved: (value) {
                     gameCode = value;
                   },
