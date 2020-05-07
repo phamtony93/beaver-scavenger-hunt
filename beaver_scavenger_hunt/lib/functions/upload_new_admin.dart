07a8bed0e3fc367997571bc9039c4f37c1ac4302
Jake Seawell <44551335+JSeawell@users.noreply.github.com>
2020-05-07 13:12:41 -0700

    Update upload_new_admin.dart

--- beaver_scavenger_hunt/lib/functions/upload_new_admin.dart
+++ beaver_scavenger_hunt/lib/functions/upload_new_admin.dart
@@ -2,8 +2,5 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 void uploadNewAdmin(String uid, String gameCode) async {
   await Firestore.instance.collection('admins').document(uid).setData({'gameID': gameCode});
-<<<<<<< HEAD
-=======
   await Firestore.instance.collection('games').document(gameCode).setData({});
->>>>>>> seawellj_branch
 }
