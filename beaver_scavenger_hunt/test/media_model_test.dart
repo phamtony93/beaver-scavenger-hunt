67fe186db06e8b6330589a00588b3ae0c0240cc7
Jake Seawell <jakeseawell@Jakes-MacBook-Pro.local>
2020-05-25 11:13:51 -0700

    Jake - fixed test

---
+++ beaver_scavenger_hunt/test/media_model_test.dart
@@ -0,0 +1,26 @@
+// This is a test package for the ClueLocation Model.
+
+import 'package:flutter_test/flutter_test.dart';
+import '../lib/models/media_model.dart';
+
+void main() {
+  test('Testing correct media model creation', (){
+
+      //create test url string
+      String test_url = "test_url";
+
+      //create test_media object from Media model/class
+      final test_media = Media();
+
+      //assert null before assigning
+      expect(test_media.url, null);
+      
+      //assert set(function) success
+      test_media.setURL(test_url);
+      expect(test_media.url, test_url);
+
+      //assert get(function) success
+      String test_get_url = test_media.getURL();
+      expect(test_get_url, test_url);
+  });
+}
